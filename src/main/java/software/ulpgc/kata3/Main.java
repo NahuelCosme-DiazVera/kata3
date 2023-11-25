7ca14a5643cc4bf38a64f9153199690942600c6c
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-25 19:21:12 +0000

    Interface HistogramDisplay created

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -9,9 +9,6 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = TsvFilePersonLoader. with("hw_25000.tsv").load();
         Map<String, Integer> histogram = new WeightHistogramCalculator(people).calculate();
-        for (String key : histogram.keySet().stream().sorted().collect(toList())) {
-            System.out.println(key + " " + histogram.get(key));
 
-        }
     }
 }
