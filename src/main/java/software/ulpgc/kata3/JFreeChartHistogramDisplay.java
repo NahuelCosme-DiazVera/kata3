a35695dfd8857d5189e61bb81d1e3b6507144be2
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-25 19:22:52 +0000

    Class JFreeChartHistogramDisplay created. Class MainFrame done

---
+++ src/main/java/software/ulpgc/kata3/JFreeChartHistogramDisplay.java
@@ -0,0 +1,10 @@
+package software.ulpgc.kata3;
+
+import javax.swing.*;
+
+public class JFreeChartHistogramDisplay extends JPanel implements HistogramDisplay {
+    @Override
+    public void show(Histogram provider) {
+
+    }
+}
