d5104ac28c9a206731d7faec940abcbb141cc4c6
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-25 19:26:08 +0000

    MainFrame instantiated

--- src/main/java/software/ulpgc/kata3/Main.java
+++ src/main/java/software/ulpgc/kata3/Main.java
@@ -9,6 +9,25 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = TsvFilePersonLoader. with("hw_25000.tsv").load();
         Map<String, Integer> histogram = new WeightHistogramCalculator(people).calculate();
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.getHistogramDisplay().show(createHistogram(histogram));
+        mainFrame.setVisible(true);
+    }
 
+    private static Histogram createHistogram(Map<String, Integer> histogram) {
+        return new Histogram() {
+            @Override
+            public int bins() {
+                return histogram.size();
+            }
+
+            @Override
+            public double[] values() {
+                return histogram.values()
+                        .stream()
+                        .mapToDouble(value -> value)
+                        .toArray();
+            }
+        };
     }
 }
